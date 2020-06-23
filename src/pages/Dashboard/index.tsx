37efaaf72aae8b635d97823e028d1a50703e31f3
marcosnto <marcos_nto@hotmail.com>
2020-06-23 11:45:48 -0300

    finalização da dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,7 @@
-import React, { useState, FormEvent } from "react";
+import React, { useState, useEffect, FormEvent } from "react";
 
 import { FiChevronRight } from "react-icons/fi";
-import { Title, Form, Repositories } from "./styles";
+import { Title, Form, Repositories, Error } from "./styles";
 import logoImg from "../../assets/logo.svg";
 
 import api from "../../services/api";
@@ -18,19 +18,41 @@ interface Repository {
 
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState("");
+  const [inputError, setInputError] = useState("");
+  const [repositories, setRepositores] = useState<Repository[]>(() => {
+    const storagedRepositories = localStorage.getItem("@GitHub: repositories");
 
-  const [repositories, setRepositores] = useState<Repository[]>([]);
+    if (storagedRepositories) {
+      return JSON.parse(storagedRepositories);
+    }
+    return [];
+  });
+
+  /** armazenar os dados no navegador */
+  useEffect(() => {
+    localStorage.setItem("@GitHub: repositories", JSON.stringify(repositories));
+  }, [repositories]);
 
   async function handleAddRepository(
     event: FormEvent<HTMLFormElement>
   ): Promise<void> {
     event.preventDefault();
-    const response = await api.get<Repository>(`/repos/${newRepo}`);
 
-    const repository = response.data;
+    if (!newRepo) {
+      setInputError("Digite o autor/nome do repositório");
+      return;
+    }
+    try {
+      const response = await api.get<Repository>(`/repos/${newRepo}`);
 
-    setRepositores([...repositories, repository]);
-    setNewRepo("");
+      const repository = response.data;
+
+      setRepositores([...repositories, repository]);
+      setNewRepo("");
+      setInputError("");
+    } catch (err) {
+      setInputError("Erro na busca por esse repositório");
+    }
   }
 
   return (
@@ -38,7 +60,7 @@ const Dashboard: React.FC = () => {
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no GitHub</Title>
 
-      <Form onSubmit={handleAddRepository}>
+      <Form hasError={Boolean(inputError)} onSubmit={handleAddRepository}>
         <input
           value={newRepo}
           onChange={(e) => setNewRepo(e.target.value)}
@@ -46,6 +68,9 @@ const Dashboard: React.FC = () => {
         />
         <button type="submit">Pesquisar</button>
       </Form>
+      {/* esse trecho quer dizer que se a variável inputError estiver
+      preenchida ele irá executar o que vier depois do &&  */}
+      {inputError && <Error>{inputError}</Error>}
 
       <Repositories>
         {repositories.map((repository) => (
